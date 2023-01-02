7e3e5fac022487c71099d9151b127a1162dda0a6
karthikeyan k <105141303+KarthikeyanKanniappan@users.noreply.github.com>
2023-01-02 17:49:14 +0530

    data modification

--- src/components/data.js
+++ src/components/data.js
@@ -22,7 +22,7 @@ const data = [
     demoLink: "https://write-weblog.netlify.app",
     source: "https://github.com/KarthikeyanKanniappan/Blog-frontEnd",
     sourceBack: "https://github.com/KarthikeyanKanniappan/Blog_server",
-    skills: ["React", "Redux", "Material-ui", "mongoose", "Node", "JWT"],
+    skills: ["React", "Redux", "mongoose", "Node", "JWT"],
   },
   {
     pic: "https://images.unsplash.com/photo-1631427962232-803d4f30c64f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
@@ -30,7 +30,7 @@ const data = [
     demoLink: "https://illustrious-elf-472acb.netlify.app",
     source: "https://github.com/KarthikeyanKanniappan/chat-app",
     sourceBack: "https://github.com/KarthikeyanKanniappan/Chat-app_server",
-    skills: ["React", "Socket.io", "Authorization", "Chakra-ui", "mongoose"],
+    skills: ["Socket.io", "React", "Authorization", "mongoose"],
   },
 ];
 
